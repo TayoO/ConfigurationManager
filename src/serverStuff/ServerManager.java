f02ddac3520aee24daa28e0103754f0b33f99a95
Tayo Odueke <todue043@uottawa.ca>
2016-06-06 16:35:17 -0400

    push configurations
    works for individual lines

--- src/serverStuff/ServerManager.java
+++ src/serverStuff/ServerManager.java
@@ -1,12 +1,14 @@
 package serverStuff;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
-
+import java.util.Scanner;
 class ServerManager
 {
-	 static String path;
+	 static String path = "c:/temp/opentext.ini";
+	 
  static SpecificMap groups= new SpecificMap<String, ServerGroup>();
  static SpecificMap servers= new SpecificMap<String, Server>();
  static SpecificMap globalSections= new SpecificMap<String, Section>();
@@ -14,6 +16,8 @@ class ServerManager
  Server[] serverList;
 public static void main (String [] args)
 {
+
+	
 	globalSections.put("default", new Section ("title"));
 	ServerManager main= new ServerManager();
 ServerGroup test= new ServerGroup("test");
@@ -79,6 +83,46 @@ public void deleteServer(String serverName)
 	groups.remove(serverName);
 }
 
+public void pushConfiguration(String [] sg, String [] s, String change)
+{
+	//sg= servergroup s= server
+	for (int i=0; i<sg.length; i++)
+	{
+		for (int j=0; j<s.length; j++)
+{
+	pushConfiguration(sg[i]+","+s[j]+","+change);
+}
 
+	}
+}
+public void pushConfiguration(String line)
+{
+	  Scanner in = new Scanner(System.in);
+		System.out.println("enter a server name, section variable and value");
+		 String server= in.nextLine();
+		 String section= in.nextLine();
+		 String variable= in.nextLine();
+		 String value= in.nextLine();
+		 
+		 try {
 
+				String content = server+","+section+","+variable+","+value+","+path;
+
+				File file = new File("U:\\test\\changes.csv");
+				// if file doesnt exists, then create it
+				if (!file.exists()) {
+					file.createNewFile();
+				}
+
+				FileWriter fw = new FileWriter(file.getAbsoluteFile());
+				BufferedWriter bw = new BufferedWriter(fw);
+				bw.append(content);
+				bw.close();
+
+				System.out.println("Done");
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+}
 }
